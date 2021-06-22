ce87419068113c47c7e6ff88616a705ad2a36744
Maria Syed <maria_s096@hotmail.com>
2021-06-22 11:32:11 +0300

    Init project, create first scene

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class FollowMouse : MonoBehaviour {
+ private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+ 
+    // Use this for initialization
+    void Start () {
+   
+    }
+   
+    // Update is called once per frame
+    void Update () {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.y = mousePosition.y < -0.9 ? mousePosition.y : -0.9f;
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+}
+ 
