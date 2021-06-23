81b80e2ad6d8a59c5d84c5e2a16337e9b7eff91b
Maria Syed <maria_s096@hotmail.com>
2021-06-23 12:36:44 +0300

    new mechanics for cooking scene

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -14,7 +14,7 @@ public class FollowMouse : MonoBehaviour {
     void Update () {
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        mousePosition.y = mousePosition.y < -0.9 ? mousePosition.y : -0.9f;
+        mousePosition.y = mousePosition.y < -3.5 ? mousePosition.y : -3.5f;
         transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
     }
 }
